3653ae796ec0b829d19de5789968311ddd75d368
Leaf <yyq920201895@163.com>
2020-03-16 20:59:37 +0800

    done i am DONE

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -5,62 +5,121 @@ Page({
    * 页面的初始数据
    */
   data: {
+    tabs: [
+      {
+        id: 0,
+        value: "体验问题",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "商品/商家投诉",
+        isActive: false
+      },
+    ],
+    chooseImgs: [],
+    // 文本域内容
+    textValue: "",
+  },
+  // 外网图片数组路径
+  UpLoadImgs: [],
 
+  // tab点击事件
+  handleTabsItemChange(e) {
+    const {
+      index
+    } = e.detail;
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    this.setData({
+      tabs
+    })
+  },
+  // 点击+选择图片
+  handleChooseImg() {
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (result) => {
+        this.setData({
+          chooseImgs: [...this.data.chooseImgs, ...result.tempFilePaths]
+        })
+      }
+    });
+  },
+  //删除图片，通过data-index传递参数
+  handleRemoveImg(e) {
+    const { index } = e.currentTarget.dataset;
+    let { chooseImgs } = this.data;
+    // 1指的是删除1个
+    chooseImgs.splice(index, 1);
+    this.setData({
+      chooseImgs
+    })
+  },
+  // 文本域输入事件
+  handleTextInpue(e) {
+    this.setData({
+      textValue: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  handleFormSubmit() {
+    const { textValue, chooseImgs } = this.data;
+    // 验证内容不为空
+    if (!textValue.trim()) {
+      wx.showToast({
+        title: '问题描述不能为空',
+        icon: 'none',
+        mask: true,
+      });
+      return;
+    }
+    wx.showLoading({
+      title: "正在上传中...",
+      mask: true,
+    });
+    // 判断有没有要上传的图片
+    if (chooseImgs.length != 0) {
+      chooseImgs.forEach((v, i) => {
+        wx.uploadFile({
+          url: 'http://tu.svvme.com/public/api',
+          filePath: v,
+          name: "file",
+          formData: {},
+          success: (result) => {
+            console.log(result);
+            let url = JSON.parse(result.data);
+            this.UpLoadImgs.push(url);
+            console.log(this.UpLoadImgs);
+            // 上传完毕后提交文本内容和图片外链到服务器,没做呢还
+            if (i === chooseImgs.length - 1) {
+              console.log("全部上传完毕");
+              this.setData({
+                textValue: "",
+                chooseImgs: []
+              })
+              // todo 调用post接口将图片外链数组和textarea上传到后台中
 
-  },
+            }
+          },
+        });
+      })
+    } else {
+      this.setData({
+        textValue: ""
+      })
+      console.log("只写了文本")
+      wx.hideLoading();
+      wx.navigateBack({
+        delta: 1
+      });
+    }
+    // 上传图片内容到服务器，每次只能上传一个，遍历数组一个个上传
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,5 +1,7 @@
 // pages/search/search.js
-import { request } from "../../request/index.js";
+import {
+  request
+} from "../../request/index.js";
 import regeneratorRuntime from "../../lib/regenerator-runtime/runtime.js"
 
 /**
@@ -10,49 +12,56 @@ import regeneratorRuntime from "../../lib/regenerator-runtime/runtime.js"
  */
 
 Page({
-    data() {
-        goods: [];
-        // 取消按钮是否显示
-        isFocus: false;
-        // 输入框值
-        inpVaule: "";
-    },
-    // 定时器
-    TimeId: -1,
+  data() {
+    goods: [];
+    // 取消按钮是否显示
+    isFocus: false;
+    // 输入框值
+    inpVaule: "";
+  },
+  // 定时器
+  TimeId: -1,
 
-    handleInput(e) {
-        const { value } = e.detail;
-        // 检查输入合法性
-        if (!value.trim()) {
-            this.setData({
-                goods: [],
-                isFocus: false
-            })
-            return;
-        }
-        this.setData({
-            isFocus: true,
-        })
-        // 设置每隔1秒钟再发送一次请求
-        clearTimeout(this.TimeId);
-        this.TimeId = setTimeout(() => {
-            this.qsearch(value);
-        }, 1000)
-
-    },
-    async qsearch(query) {
-        const result = await request({ url: "/goods/qsearch", data: { query } });
-        console.log(result);
-        this.setData({
-            goods: result.data.message
-        })
-    },
-    async handleCancel() {
-        this.setData({
-            inpVaule: "",
-            isFocus: false,
-            goods: []
-        })
+  handleInput(e) {
+    const {
+      value
+    } = e.detail;
+    // 检查输入合法性
+    if (!value.trim()) {
+      this.setData({
+        goods: [],
+        isFocus: false
+      })
+      return;
     }
+    this.setData({
+      isFocus: true,
+    })
+    // 设置每隔1秒钟再发送一次请求
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.qsearch(value);
+    }, 1000)
+
+  },
+  async qsearch(query) {
+    const result = await request({
+      url: "/goods/qsearch",
+      data: {
+        query
+      }
+    });
+    console.log(result);
+    this.setData({
+      goods: result.data.message
+    })
+  },
+  async handleCancel() {
+    this.setData({
+      inpVaule: "",
+      isFocus: false,
+      goods: []
+    })
+  }
 
 })
