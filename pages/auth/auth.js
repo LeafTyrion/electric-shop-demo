65275c4299b727f7c6bd8dca58082e55b8eb1754
Leaf <yyq920201895@163.com>
2020-04-27 21:43:13 +0800

    nothing to do

--- pages/auth/auth.js
+++ pages/auth/auth.js
@@ -1,15 +1,36 @@
 // pages/auth/auth.js
-import { login } from "../../utils/asyncWX.js"
+import {
+  login
+} from "../../utils/asyncWX.js"
 import regeneratorRuntime from "../../lib/regenerator-runtime/runtime.js"
-import { request } from "../../request/index.js"
+import {
+  request
+} from "../../request/index.js"
 Page({
   async handleGetUserInfo(e) {
     try {
-      const { encryptedData, rawData, iv, signature } = e.detail;
-      const { code } = await login();
-      const loginParams = { encryptedData, rawData, iv, signature, code };
+      const {
+        encryptedData,
+        rawData,
+        iv,
+        signature
+      } = e.detail;
+      const {
+        code
+      } = await login();
+      const loginParams = {
+        encryptedData,
+        rawData,
+        iv,
+        signature,
+        code
+      };
       console.log(loginParams);
-      const res = await request({ url: "/users/wxlogin", data: loginParams, method: "post" });
+      const res = await request({
+        url: "/users/wxlogin",
+        data: loginParams,
+        method: "post"
+      });
       console.log(res);
       wx.setStorageSync("token", res.data.token);
       wx.setStorageSync("token", "fuckyou!");
